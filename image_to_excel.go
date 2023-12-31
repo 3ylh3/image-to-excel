69b07aa1bea5150af35435d23a6ca2123a0bed8f
3ylh3 <781706889@qq.com>
2023-12-31 15:39:30 +0800

    并发处理

--- image_to_excel.go
+++ image_to_excel.go
@@ -11,7 +11,10 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 )
+
+var colorIndex int
 
 func main() {
 	// 图片路径
@@ -60,7 +63,7 @@ func main() {
 	// Set active sheet of the workbook.
 	ef.SetActiveSheet(index)
 	colorMap := make(map[string]int)
-	tmp := 0
+	colorIndex = 0
 	lengthNum := length / pix
 	if length%pix != 0 {
 		lengthNum++
@@ -69,44 +72,21 @@ func main() {
 	if height%pix != 0 {
 		height++
 	}
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
 	// 遍历横格子
 	for i := 0; i < lengthNum; i++ {
 		// 遍历竖格子
 		for j := 0; j < heightNum; j++ {
-			num := 0
-			// 计算该格子像素点
-			x := i*pix + 5
-			y := j*pix + 5
-			r, g, b, a := img.At(x, y).RGBA()
-			rgba := color.RGBA{R: uint8(r >> 8), G: uint8(g >> 8), B: uint8(b >> 8), A: uint8(a >> 8)}
-			colorStr := rgbToHex(rgba.R, rgba.G, rgba.B)
-			v, ok := colorMap[colorStr]
-			if !ok {
-				// 该颜色第一次出现
-				tmp++
-				colorMap[colorStr] = tmp
-				num = tmp
-			} else {
-				num = v
-			}
-			// 写入excel
-			style, _ := ef.NewStyle(&excelize.Style{
-				Border: []excelize.Border{
-					{Type: "top", Color: "#000000", Style: 1},
-					{Type: "bottom", Color: "#000000", Style: 1},
-					{Type: "left", Color: "#000000", Style: 1},
-					{Type: "right", Color: "#000000", Style: 1},
-				},
-				Fill: excelize.Fill{Type: "pattern", Color: []string{colorStr}, Pattern: 1},
-			})
-			_ = ef.SetCellValue("Sheet1", xMap[i+1]+strconv.Itoa(j+1), num)
-			_ = ef.SetCellStyle("Sheet1", xMap[i+1]+strconv.Itoa(j+1), xMap[i+1]+strconv.Itoa(j+1), style)
-			// 设置行高
-			_ = ef.SetRowHeight("Sheet1", j+1, cellHeight)
+			indexI := i
+			indexJ := j
+			wg.Add(1)
+			go process(&wg, &mutex, indexI, indexJ, pix, img, colorMap, ef, xMap, cellHeight)
 		}
-		// 设置列宽
-		_ = ef.SetColWidth("Sheet1", xMap[i+1], xMap[i+1], cellHeight*0.3528/2.2733)
 	}
+	wg.Wait()
+	// 设置列宽
+	_ = ef.SetColWidth("Sheet1", xMap[1], xMap[lengthNum], cellHeight*0.3528/2.2733)
 	// Save spreadsheet by the given path.
 	if err := ef.SaveAs("./result.xlsx"); err != nil {
 		log.Printf("save excel file error:%v", err)
@@ -138,6 +118,43 @@ func columnName(n int) string {
 		n /= 26
 	}
 	return colName
+}
+
+func process(wg *sync.WaitGroup, mutex *sync.Mutex, i int, j int, pix int, img image.Image, colorMap map[string]int, ef *excelize.File, xMap map[int]string, cellHeight float64) {
+	defer wg.Done()
+	num := 0
+	// 计算该格子像素点
+	x := i*pix + pix/2
+	y := j*pix + pix/2
+	r, g, b, a := img.At(x, y).RGBA()
+	rgba := color.RGBA{R: uint8(r >> 8), G: uint8(g >> 8), B: uint8(b >> 8), A: uint8(a >> 8)}
+	colorStr := rgbToHex(rgba.R, rgba.G, rgba.B)
+	// 加锁
+	mutex.Lock()
+	defer mutex.Unlock()
+	v, ok := colorMap[colorStr]
+	if !ok {
+		// 该颜色第一次出现
+		colorIndex++
+		colorMap[colorStr] = colorIndex
+		num = colorIndex
+	} else {
+		num = v
+	}
+	// 写入excel
+	style, _ := ef.NewStyle(&excelize.Style{
+		Border: []excelize.Border{
+			{Type: "top", Color: "#000000", Style: 1},
+			{Type: "bottom", Color: "#000000", Style: 1},
+			{Type: "left", Color: "#000000", Style: 1},
+			{Type: "right", Color: "#000000", Style: 1},
+		},
+		Fill: excelize.Fill{Type: "pattern", Color: []string{colorStr}, Pattern: 1},
+	})
+	_ = ef.SetCellValue("Sheet1", xMap[i+1]+strconv.Itoa(j+1), num)
+	_ = ef.SetCellStyle("Sheet1", xMap[i+1]+strconv.Itoa(j+1), xMap[i+1]+strconv.Itoa(j+1), style)
+	// 设置行高
+	_ = ef.SetRowHeight("Sheet1", j+1, cellHeight)
 }
 
 // rgb转16进制
